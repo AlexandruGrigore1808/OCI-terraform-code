f403d3750869b882fa93a723404886d52e1327cc
Alex Grigore <alexandru.grigore1808@gmail.com>
2021-07-01 13:19:29 +0300

    Initial files

---
+++ modules/compute/variables.tf
@@ -0,0 +1,3 @@
+variable "compartment_ocid" {
+  default = "ocid1.compartment.oc1..aaaaaaaaqz3bk2jky4745msubtxlwwkqbxqh2kteay7vavfib6or4hxiirha"
+}
---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaibrpsc2fhjxvmbla6gu327mkldqwaabj37gzhgorkfw4q46tznva"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaaioxqoaog6wz4rs7j5wgw54dgwq5ptpjzzekfrqtdjjtantpvmhuq"
+  fingerprint      = "7f:e2:6b:7d:18:92:10:2b:21:b5:a9:27:b1:39:97:29"
+  private_key_path = "/mnt/c/workspace/keys/private_key.pem"
+  region           = "eu-frankfurt-1"
+}
